d1d85364f3caa6bd4e0ee6b322c61cf56e41fa28
mgreiff <marcusgreiff.93@hotmail.com>
2017-09-01 21:15:58 +0200

    First attempt to add documentationO

---
+++ docs/make.jl
@@ -0,0 +1,17 @@
+using Documenter, LabProcesses
+# makedocs()
+# deploydocs(
+# deps   = Deps.pip("pygments", "mkdocs", "python-markdown-math", "mkdocs-cinder"),
+# repo   = "gitlab.control.lth.se/processes/LabProcesses.jl",
+# branch = "gh-pages",
+# julia  = "0.6",
+# osname = "linux"
+# )
+
+makedocs(
+    format = :html,
+    sitename = "LabConnections",
+    pages = [
+        "index.md",
+    ]
+)
---
+++ docs/src/index.md
@@ -0,0 +1,37 @@
+# LabConnections
+
+```@contents
+Depth = 3
+```
+
+asd
+
+## Installation
+asd
+
+# How to implement a new process
+asd
+
+# How to control a process
+asd
+
+# How to implement a Simulated Process
+asd
+
+## Linear process
+asd
+
+asd
+
+# Process interface specification
+asd
+
+```@autodocs
+Modules = [LabConnections]
+Private = false
+```
+
+# Index
+
+```@index
+```
