7751d6f7983f3e942f7074f2b1a15d51085d6579
mgreiff <marcusgreiff.93@hotmail.com>
2017-09-01 20:57:46 +0200

    Temporarily move the BB tests until the BB filesystem has been imported

---
+++ test/BeagleBone/runtests.jl
@@ -0,0 +1,6 @@
+using LabConnections
+using Base.Test
+
+include("PWM_test.jl")
+include("Sys_LED_test.jl")
+include("GPIO_test.jl")
--- test/runtests.jl
+++
@@ -1,6 +0,0 @@
-using LabConnections
-using Base.Test
-
-include("BeagleBone/PWM_test.jl")
-include("BeagleBone/Sys_LED_test.jl")
-include("BeagleBone/GPIO_test.jl")
